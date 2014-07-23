419562ab41090eb320775dbd1c6324985aa747c7
Jef Spaleta <jdspaleta@alaska.edu>
2014-07-22 20:40:11 -0800

    Fix how the Python.h include is to make sure its the first include in the C files which call rtypes.h

--- pydarn/rst/src/dmap.c
+++ pydarn/rst/src/dmap.c
@@ -27,6 +27,7 @@
   
 */ 
 
+#include <Python.h>
 #include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
--- pydarn/rst/src/dmapio.c
+++ pydarn/rst/src/dmapio.c
@@ -16,8 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
  
-#include <stdio.h>
 #include <Python.h>
+#include <stdio.h>
 #include <datetime.h>
 #include <stdlib.h>
 #include <sys/stat.h>
